b8412227dab59eebe022a1da3c0e83d144ec08dd
victoriamsm <victoriamsm35@gmail.com>
2023-09-02 08:09:07 -0300

    ajustando ex1

--- semana3-exercicios/exercicio1-carro.py
+++ semana3-exercicios/exercicio1-carro.py
@@ -1,38 +1,49 @@
 class Carro():
-    def __init__(self): # MÉTODO / ATRIBUTO
+    def __init__(self, cor, modelo): # MÉTODO / ATRIBUTO
         self.ligado = False
-        self.cor = 'Azul'
-        self.modelo = 'Evoque SUV'
+        self.cor = cor
+        self.modelo = modelo
         self.velocidade = 0
-        self.velocidade_max = 230
+        self.velocidade_max = 200
         self.velocidade_min = 0
+
     def ligar(self):  #COMPORTAMENTO
         self.ligado = True 
+
     def desligar(self):
         self.ligado = False
+        self.velocidade = 0.0
+
     def acelerar(self):
         if not self.ligado:
             return
         if self.velocidade < self.velocidade_max:
             self.velocidade += 10
+
     def desacelerar(self):
         if not self.ligado:
             return
         if self.velocidade > self.velocidade_min:
             self.velocidade -= 10
-    def __str__(self) -> str:
-        return f'-Ligado: {self.ligado} -Cor: {self.cor} -Modelo: {self.modelo} Velocidade atual: {self.velocidade}'
 
-carro1 = Carro()
+    def __str__(self) -> str:
+        ligado_str = "ligado" if self.ligado == True else "desligado"
+        return f'O carro da cor {self.cor} e modelo {self.modelo} está {ligado_str} com a velocidade atual de {self.velocidade}KM!'
+
+carro1 = Carro("Azul", "Evoque")
 
 carro1.ligar()
-print(f'O carro está ligado? {carro1.ligado}')
+print(carro1)
+print('-='*15)
+
 
 print(f'Acelerando carro: ',end='')
 for i in range(8):
     carro1.acelerar()
     print(f'{carro1.velocidade}...',end='')
 print(' [VELOCIDADE MÁXIMA DA VIA!]')
+print(carro1)
+print('-='*15)
 
 print(f'Desacelerando o carro: ',end='')
 for i in range(8):
@@ -41,6 +52,5 @@ for i in range(8):
 print(' [PAROU!]')
 
 carro1.desligar()
-
 
 print('Carro: ', carro1)
